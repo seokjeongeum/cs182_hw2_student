83e60a8ed146ed5558ed3ebe5c6a13eaf84467bc
root <jeseok@dblab.postech.ac.kr>
2025-02-10 09:11:10 +0900

    Refactor CaptioningRNN initialization and implement loss and sampling methods

--- deeplearning/classifiers/rnn.py
+++ deeplearning/classifiers/rnn.py
@@ -18,8 +18,15 @@ class CaptioningRNN(object):
     Note that we don't use any regularization for the CaptioningRNN.
     """
 
-    def __init__(self, word_to_idx, input_dim=512, wordvec_dim=128,
-                 hidden_dim=128, cell_type='rnn', dtype=np.float32):
+    def __init__(
+        self,
+        word_to_idx,
+        input_dim=512,
+        wordvec_dim=128,
+        hidden_dim=128,
+        cell_type="rnn",
+        dtype=np.float32,
+    ):
         """
         Construct a new CaptioningRNN instance.
 
@@ -33,7 +40,7 @@ class CaptioningRNN(object):
         - dtype: numpy datatype to use; use float32 for training and float64 for
           numeric gradient checking.
         """
-        if cell_type not in {'rnn', 'lstm'}:
+        if cell_type not in {"rnn", "lstm"}:
             raise ValueError('Invalid cell_type "%s"' % cell_type)
 
         self.cell_type = cell_type
@@ -44,36 +51,35 @@ class CaptioningRNN(object):
 
         vocab_size = len(word_to_idx)
 
-        self._null = word_to_idx['<NULL>']
-        self._start = word_to_idx.get('<START>', None)
-        self._end = word_to_idx.get('<END>', None)
+        self._null = word_to_idx["<NULL>"]
+        self._start = word_to_idx.get("<START>", None)
+        self._end = word_to_idx.get("<END>", None)
 
         # Initialize word vectors
-        self.params['W_embed'] = np.random.randn(vocab_size, wordvec_dim)
-        self.params['W_embed'] /= 100
+        self.params["W_embed"] = np.random.randn(vocab_size, wordvec_dim)
+        self.params["W_embed"] /= 100
 
         # Initialize CNN -> hidden state projection parameters
-        self.params['W_proj'] = np.random.randn(input_dim, hidden_dim)
-        self.params['W_proj'] /= np.sqrt(input_dim)
-        self.params['b_proj'] = np.zeros(hidden_dim)
+        self.params["W_proj"] = np.random.randn(input_dim, hidden_dim)
+        self.params["W_proj"] /= np.sqrt(input_dim)
+        self.params["b_proj"] = np.zeros(hidden_dim)
 
         # Initialize parameters for the RNN
-        dim_mul = {'lstm': 4, 'rnn': 1}[cell_type]
-        self.params['Wx'] = np.random.randn(wordvec_dim, dim_mul * hidden_dim)
-        self.params['Wx'] /= np.sqrt(wordvec_dim)
-        self.params['Wh'] = np.random.randn(hidden_dim, dim_mul * hidden_dim)
-        self.params['Wh'] /= np.sqrt(hidden_dim)
-        self.params['b'] = np.zeros(dim_mul * hidden_dim)
+        dim_mul = {"lstm": 4, "rnn": 1}[cell_type]
+        self.params["Wx"] = np.random.randn(wordvec_dim, dim_mul * hidden_dim)
+        self.params["Wx"] /= np.sqrt(wordvec_dim)
+        self.params["Wh"] = np.random.randn(hidden_dim, dim_mul * hidden_dim)
+        self.params["Wh"] /= np.sqrt(hidden_dim)
+        self.params["b"] = np.zeros(dim_mul * hidden_dim)
 
         # Initialize output to vocab weights
-        self.params['W_vocab'] = np.random.randn(hidden_dim, vocab_size)
-        self.params['W_vocab'] /= np.sqrt(hidden_dim)
-        self.params['b_vocab'] = np.zeros(vocab_size)
+        self.params["W_vocab"] = np.random.randn(hidden_dim, vocab_size)
+        self.params["W_vocab"] /= np.sqrt(hidden_dim)
+        self.params["b_vocab"] = np.zeros(vocab_size)
 
         # Cast parameters to correct dtype
         for k, v in self.params.items():
             self.params[k] = v.astype(self.dtype)
-
 
     def loss(self, features, captions):
         """
@@ -100,20 +106,20 @@ class CaptioningRNN(object):
         captions_out = captions[:, 1:]
 
         # You'll need this
-        mask = (captions_out != self._null)
+        mask = captions_out != self._null
 
         # Weight and bias for the affine transform from image features to initial
         # hidden state
-        W_proj, b_proj = self.params['W_proj'], self.params['b_proj']
+        W_proj, b_proj = self.params["W_proj"], self.params["b_proj"]
 
         # Word embedding matrix
-        W_embed = self.params['W_embed']
+        W_embed = self.params["W_embed"]
 
         # Input-to-hidden, hidden-to-hidden, and biases for the RNN
-        Wx, Wh, b = self.params['Wx'], self.params['Wh'], self.params['b']
+        Wx, Wh, b = self.params["Wx"], self.params["Wh"], self.params["b"]
 
         # Weight and bias for the hidden-to-vocab transformation.
-        W_vocab, b_vocab = self.params['W_vocab'], self.params['b_vocab']
+        W_vocab, b_vocab = self.params["W_vocab"], self.params["b_vocab"]
 
         loss, grads = 0.0, {}
         ############################################################################
@@ -137,13 +143,27 @@ class CaptioningRNN(object):
         # defined above to store loss and gradients; grads[k] should give the      #
         # gradients for self.params[k].                                            #
         ############################################################################
-        pass
+        out_proj, cache_proj = affine_forward(features, W_proj, b_proj)
+        out_embed, cache_embed = word_embedding_forward(captions_in, W_embed)
+        if self.cell_type == "rnn":
+            out, cache = rnn_forward(out_embed, out_proj, Wx, Wh, b)
+        if self.cell_type == "lstm":
+            raise NotImplementedError()
+        out_vocab, cache_vocab = temporal_affine_forward(out, W_vocab, b_vocab)
+        loss, dx = temporal_softmax_loss(out_vocab, captions_out, mask)
+
+        dx, grads["W_vocab"], grads["b_vocab"] = temporal_affine_backward(dx, cache_vocab)
+        if self.cell_type == "rnn":
+            dx, dh0, grads["Wx"], grads["Wh"], grads["b"] = rnn_backward(dx, cache)
+        if self.cell_type == "lstm":
+            raise NotImplementedError()
+        grads["W_embed"] = word_embedding_backward(dx, cache_embed)
+        dh0, grads["W_proj"], grads["b_proj"] = affine_backward(dh0, cache_proj)
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
 
         return loss, grads
-
 
     def sample(self, features, max_length=30):
         """
@@ -173,10 +193,10 @@ class CaptioningRNN(object):
         captions = self._null * np.ones((N, max_length), dtype=np.int32)
 
         # Unpack parameters
-        W_proj, b_proj = self.params['W_proj'], self.params['b_proj']
-        W_embed = self.params['W_embed']
-        Wx, Wh, b = self.params['Wx'], self.params['Wh'], self.params['b']
-        W_vocab, b_vocab = self.params['W_vocab'], self.params['b_vocab']
+        W_proj, b_proj = self.params["W_proj"], self.params["b_proj"]
+        W_embed = self.params["W_embed"]
+        Wx, Wh, b = self.params["Wx"], self.params["Wh"], self.params["b"]
+        W_vocab, b_vocab = self.params["W_vocab"], self.params["b_vocab"]
 
         ###########################################################################
         # TODO: Implement test-time sampling for the model. You will need to      #
@@ -199,7 +219,16 @@ class CaptioningRNN(object):
         # functions; you'll need to call rnn_step_forward or lstm_step_forward in #
         # a loop.                                                                 #
         ###########################################################################
-        pass
+        x = self._start
+        h, _ = affine_forward(features, W_proj, b_proj)
+        for t in range(max_length):
+            out, _ = word_embedding_forward(x, W_embed)
+            if self.cell_type == "rnn":
+                h, _ = rnn_step_forward(out, h, Wx, Wh, b)
+            if self.cell_type == "lstm":
+                raise NotImplementedError()
+            out, _ = affine_forward(h, W_vocab, b_vocab)
+            captions[:, t] = out.argmax(axis=1)
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
